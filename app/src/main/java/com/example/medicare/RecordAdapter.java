198e1212a9d6bfb1552795948d872a46d42aae24
BiodunO <56849994+BiodunO@users.noreply.github.com>
2019-10-23 00:32:23 +0100

    Update RecordAdapter.java
    In reference to Line 67... I need to upload a right_to_left.xml file, but the privilege has not been granted, so i left resources as one of the previous ones ... slidein.xml
    
    In summary
    I Added codes to RecordAdapter.java to include animation of Binded data.
    and was meant to Include right_to_left.xml in Anim directory, but can upload

--- app/src/main/java/com/example/medicare/RecordAdapter.java
+++ app/src/main/java/com/example/medicare/RecordAdapter.java
@@ -4,24 +4,32 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RecordAdapter extends RecyclerView.Adapter<RecordAdapter.RecordsViewHolder> {
 
     List<RecordModel> recordModelList ;
     Context context;
+
+    //Added 2 lines  (Biodun)
+    LinearLayout container;
+    Animation  animation;
+
     public class RecordsViewHolder extends RecyclerView.ViewHolder{
 
         public TextView tvAge;
         public TextView tvDate;
         public TextView tvfullNames;
         public TextView tvPurpose;
+
+
 
         public RecordsViewHolder( View itemView) {
             super(itemView);
@@ -30,6 +38,11 @@ public class RecordAdapter extends RecyclerView.Adapter<RecordAdapter.RecordsVie
             tvDate = itemView.findViewById(R.id.tvrecordDate);
             tvfullNames = itemView.findViewById(R.id.tvrecordFullnames);
             tvPurpose = itemView.findViewById(R.id.tvrecordPurpose);
+
+            //called reference to layout(Biodun)
+            container = itemView.findViewById(R.id.layoutid);
+
+
 
         }
     }
@@ -49,7 +62,13 @@ public class RecordAdapter extends RecyclerView.Adapter<RecordAdapter.RecordsVie
 
     @Override
     public void onBindViewHolder(RecordsViewHolder holder, int position) {
-       RecordModel recordModel = recordModelList.get(position);
+
+        //Added 2 lines (Biodun)
+        animation = AnimationUtils.loadAnimation(context,R.anim.slidein);
+        container.startAnimation(animation);
+
+
+        RecordModel recordModel = recordModelList.get(position);
         holder.tvAge.setText("Age:"+ " "+ recordModel.getAge());
         holder.tvDate.setText("Date Visited: "+" "+recordModel.getRecordDate());
         holder.tvfullNames.setText("Full Names:" +" "+recordModel.getFullnames());
